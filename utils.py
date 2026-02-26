2a9830264e29ac542388b2193d857d76133c91e5
Dawid Szyjka <dawid.szyjkaa1910@interia.pl>
2026-02-26 20:52:22 +0100

    Refactor: move reusable code to utils.py

---
+++ utils.py
@@ -0,0 +1,69 @@
+from torch.utils.data import Dataset
+from torchvision import transforms
+from sklearn.metrics import classification_report, ConfusionMatrixDisplay, confusion_matrix
+import matplotlib.pyplot as plt
+import torch
+
+
+class MedicalDataset(Dataset):
+
+    def __init__(self, images, labels, transform=None):
+        self.images = images
+        self.labels = labels
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.images)
+    
+    def __getitem__(self, idx):
+        image = self.images[idx]
+        label = self.labels[idx]
+
+        if self.transform is not None:
+            image = self.transform(image)
+
+        return image, label
+    
+def evaluate_model(model, device, loader):
+    y_pred, y_true = [], []
+
+    model.eval()
+    with torch.no_grad():
+        for inputs, labels in loader:
+            inputs = inputs.to(device)
+            labels = labels.to(device).squeeze()
+
+            outputs = model(inputs)
+            _, pred = torch.max(outputs, 1)
+
+            y_pred.extend(pred.cpu().numpy())
+            y_true.extend(labels.cpu().numpy())
+
+    print(classification_report(y_true, y_pred))
+
+    cm = confusion_matrix(y_true, y_pred)
+    disp = ConfusionMatrixDisplay(cm)
+    disp.plot(cmap=plt.cm.Blues)
+
+    plt.show()
+
+train_transform = transforms.Compose([
+    transforms.RandomHorizontalFlip(p=0.5),
+    transforms.RandomRotation(degrees=20),
+])
+
+label2organ = {
+  1: "Other Posterior Circulation",
+  2: "Basilar Tip",
+  3: "Right Posterior Communicating Artery",
+  4: "Left Posterior Communicating Artery",
+  5: "Right Infraclinoid Internal Carotid Artery",
+  6: "Left Infraclinoid Internal Carotid Artery",
+  7: "Right Supraclinoid Internal Carotid Artery",
+  8: "Left Supraclinoid Internal Carotid Artery",
+  9: "Right Middle Cerebral Artery",
+  10: "Left Middle Cerebral Artery",
+  11: "Right Anterior Cerebral Artery",
+  12: "Left Anterior Cerebral Artery",
+  13: "Anterior Communicating Artery"
+}
